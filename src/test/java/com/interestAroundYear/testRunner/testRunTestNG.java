a2cb0584d473e2f3327050eae4cc85b8c1c737ee
Kaustubh <kolekarkaustubh@gmail.com>
2024-02-29 20:20:44 +0530

    test

--- src/test/java/com/interestAroundYear/testRunner/testRunTestNG.java
+++ src/test/java/com/interestAroundYear/testRunner/testRunTestNG.java
@@ -1,17 +1,5 @@
 package com.interestAroundYear.testRunner;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-
-import com.interestAroundYear.factory.helperClass;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
